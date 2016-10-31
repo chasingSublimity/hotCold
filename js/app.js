810cc04a47a22ab8a465e3f864beafdc134b8a6f
chasingSublimity <blakemsager@gmail.com>
2016-10-30 20:32:57 -0500

    added error handling

--- js/app.js
+++ js/app.js
@@ -1,42 +1,47 @@
 
 $(document).ready(function(){
 
-	/* Functions */
+	/* Random Integer Function */
 	function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 	};
 
+	/* Comparison Function */
 	function compareNumbers(number1, number2) {
 		var difference = Math.abs(number1 - number2);
-		if (difference >= 50) {
-			$("#feedback").text("cold")
+
+		/* Error Handling */
+		if (number1 < 1 || number1 > 100 || isNaN(number1) === true) {
+			alert("Invalid Input: Please enter a number larger than 1 and less than 100.");
+		} else if (difference >= 50) {
+			$("#feedback").text("cold");
+			trackGuess();
 		} else if (difference >= 30) {
-			$("#feedback").text("kinda warm")
+			$("#feedback").text("kinda warm");
+			trackGuess();
 		} else if (difference >= 10) {
-			$("#feedback").text("hot")
+			$("#feedback").text("hot");
+			trackGuess();
 		} else if (difference === 0) {
 			$("#feedback").text("You Won. Click new game to play again");
 			$("#guessButton").css('opacity','0');
 		};
 	};
 
-	function errorMessages(guess) {
-		if (guess < 1 || guess > 100) {
-			alert("Please enter a number larger than 1 and less than 100");
-		};
-
+	/* Guess Tracking Function */
+	function trackGuess() {
+		var guess = parseInt($("#userGuess").val());
+		var liTemplate = '<li>' + guess + '</li>';
+		$("#guessList").append(liTemplate);
 	};
-
 
 	/* Generate Random Number on Page Load */
   	var randomInt = getRandomInt(1,20);
   	console.log(randomInt);
 
-
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -49,24 +54,13 @@ $(document).ready(function(){
   		location.reload();
   	});
 
-  	/* Guess button functions */
+  	/* On Guess Button Click */
   	$("form").submit(function(event){
   		event.preventDefault();
   		var guess = parseInt($("#userGuess").val());
-  		errorMessages(guess);
   		compareNumbers(guess, randomInt);
-  		var liTemplate = '<li>' + guess + '</li>';
-
-  		function trackGuess() {
-		$("#guessList").append(liTemplate);
-	};
-  		trackGuess();
   		$(this).closest('form').find("input[type=text], textarea").val("");
   	});
-
-
-
-
 });
 
 
